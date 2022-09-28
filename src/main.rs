d913f420bfa5f6aa391ab44d2a9089a459e75fcf
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-28 14:08:57 +0530

    Scripted auto-commit on change (2022-09-28 14:08:57) by gitwatch.sh

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
 
 async fn health_check(req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
@@ -14,8 +10,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
         .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
 
     })
     .bind("127.0.0.1:8000")?
