4bc25f756666be485bb26256c43ae2fc2d3ffa3c
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-28 14:40:28 +0530

    Scripted auto-commit on change (2022-09-28 14:40:28) by gitwatch.sh

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use std::io;
+
+use actix_web::{web, App, HttpResponse, HttpServer, dev::Server};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -7,9 +9,9 @@ async fn health_check() -> HttpResponse {
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub async fn run() -> std::io::Result<()> {
+pub async fn run() -> Result<Server,std::io::Error> {
    let server =  HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
         .run();
-ok(server)
+    ok(server)
 }
