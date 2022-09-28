51e7cbe11d87c98528d4ed8af156d6ed349a2204
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-28 14:08:31 +0530

    Scripted auto-commit on change (2022-09-28 14:08:31) by gitwatch.sh

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,10 @@ async fn health_check(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+        .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
+
     })
     .bind("127.0.0.1:8000")?
     .run()
