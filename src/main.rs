648fe350177e7c67c373a101e89a252b96e62984
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-28 14:22:02 +0530

    Scripted auto-commit on change (2022-09-28 14:22:02) by gitwatch.sh

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 
-
-async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+use rust_blog::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-        .route("/health_check", web::get().to(health_check))
-
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
