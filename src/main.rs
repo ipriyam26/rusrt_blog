f27283bac0e2f34d7c5d903dd7b159bc9e08a72f
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-28 14:03:58 +0530

    Scripted auto-commit on change (2022-09-28 14:03:58) by gitwatch.sh

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
-async fn health_check(req: HttpRequest) -> impl 
+async fn health_check(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
