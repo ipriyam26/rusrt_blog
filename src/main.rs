5c506983b07927a63c186a5fd6a28953c96d8436
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-28 14:41:54 +0530

    Scripted auto-commit on change (2022-09-28 14:41:54) by gitwatch.sh

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use rust_blog::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
     run()?.await
 }
