d6a6fd6027bf2145a0a96ecd8d2698de28fed2cf
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-03 13:16:33 +0530

    Scripted auto-commit on change (2022-10-03 13:16:33) by gitwatch.sh

--- src/tests/health_check.rs
+++ src/tests/health_check.rs
@@ -19,7 +19,7 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background ~somehow~
-async fn spawn_app() -> std::io::Result<()> {
+fn spawn_app() -> std::io::Result<()> {
 let server = rust_blog::run().expect("Failed to bind address");
 let _ = tokio::spawn(server);
 }
